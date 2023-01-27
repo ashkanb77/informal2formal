6d8f904dba8848f9ba9c55cc7bd6b34da7c49152
ashkan <bagheriashkan77@gmail.com>
2023-01-27 18:10:05 +0330

    Add files via upload

--- test.py
+++ test.py
@@ -7,19 +7,22 @@ from utils import find_formal_forms, read_dict
 
 parser = ArgumentParser()
 
-parser.add_argument('--tokenizer', type=str, default='Ahmad/parsT5-base', help='tokenizer')
+parser.add_argument('--tokenizer', type=str, default='erfan226/persian-t5-paraphraser', help='tokenizer')
+parser.add_argument('--model', type=str, default='erfan226/persian-t5-paraphraser', help='model')
 parser.add_argument('--model_checkpoint', type=str, default='model.pth', help='dataset directory')
-parser.add_argument('--dict_path', type=str, default='model.pth', help='dictionary path')
+parser.add_argument('--dict_path', type=str, default='dataset/dict.csv', help='dictionary path')
 parser.add_argument('--min_count', type=int, default=10, help='min count ')
 parser.add_argument('--informal_texts', type=str, default='informals.txt')
+parser.add_argument('--append_formals', type=bool, default=False,
+                    help='append formal forms to end of informal sentence')
 
 args = parser.parse_args()
 tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-model = FormalModule(args.model_checkpoint)
-model.load_state_dict(torch.load(args.model))
+model = FormalModule(args.model)
+model.load_state_dict(torch.load(args.model_checkpoint))
 model.to(device)
 
 
@@ -29,15 +32,16 @@ def convert(text):
       padding=True,
       return_tensors='pt'
     )
+    text_encoding = {k: v.to(device) for k, v in text_encoding.items()}
 
-    generated_ids = model.model.generate(
+    generated_ids = model.t5_model.generate(
       input_ids=text_encoding['input_ids'],
       attention_mask=text_encoding['attention_mask'],
       early_stopping=True
     )
 
     preds = [
-           tokenizer.decode_batch(gen_id, skip_special_tokens=True,
+           tokenizer.decode(gen_id, skip_special_tokens=True,
                             clean_up_tokenization_spaces=True) for gen_id in generated_ids
     ]
 
@@ -51,10 +55,13 @@ with open(args.informal_texts, 'r') as file:
 
 
 dic = read_dict(args.dict_path, args.min_count)
-sentences = find_formal_forms(sentences, dic)
+
+if args.append_formals:
+    sentences = find_formal_forms(sentences, dic)
 
 res = []
 for sentence in sentences:
     res.append(convert(sentence))
 
-print(res)
+for r in res:
+    print(r)
