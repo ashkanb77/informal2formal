29bd758914deb09b98bbc1f44d99188a81425181
ashkan <bagheriashkan77@gmail.com>
2023-02-04 18:25:12 +0330

    Add files via upload

--- test.py
+++ test.py
@@ -2,17 +2,24 @@ import torch
 from argparse import ArgumentParser
 from model import FormalModule
 from transformers import AutoTokenizer
-from utils import find_formal_forms, read_dict
-
+from utils import find_formal_forms, read_dataset
+from datasets import load_metric
+from dataset import FormalDataset
+from torch.utils.data import DataLoader
+from utils import collate_fn
+import pandas as pd
+from rouge import Rouge
+from tqdm import tqdm
 
 parser = ArgumentParser()
 
 parser.add_argument('--tokenizer', type=str, default='erfan226/persian-t5-paraphraser', help='tokenizer')
+parser.add_argument('--batch_size', type=int, default=16, help='batch size')
 parser.add_argument('--model', type=str, default='erfan226/persian-t5-paraphraser', help='model')
 parser.add_argument('--model_checkpoint', type=str, default='model.pth', help='dataset directory')
 parser.add_argument('--dict_path', type=str, default='dataset/dict.csv', help='dictionary path')
 parser.add_argument('--min_count', type=int, default=10, help='min count ')
-parser.add_argument('--informal_texts', type=str, default='informals.txt')
+parser.add_argument('--validation_path', type=str, default='val_informals.csv')
 parser.add_argument('--append_formals', type=bool, default=False,
                     help='append formal forms to end of informal sentence')
 
@@ -25,43 +32,64 @@ model = FormalModule(args.model)
 model.load_state_dict(torch.load(args.model_checkpoint))
 model.to(device)
 
+rouge = Rouge()
 
-def convert(text):
-    text_encoding = tokenizer(
-      text,
-      padding=True,
-      return_tensors='pt'
-    )
-    text_encoding = {k: v.to(device) for k, v in text_encoding.items()}
 
+def convert_compute(input_ids, attention_mask, labels):
     generated_ids = model.t5_model.generate(
-      input_ids=text_encoding['input_ids'],
-      attention_mask=text_encoding['attention_mask'],
-      early_stopping=True
+        input_ids=input_ids,
+        attention_mask=attention_mask, max_new_tokens='longest'
     )
 
-    preds = [
-           tokenizer.decode(gen_id, skip_special_tokens=True,
-                            clean_up_tokenization_spaces=True) for gen_id in generated_ids
-    ]
+    pred_str = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+    input_str = tokenizer.batch_decode(input_ids, skip_special_tokens=True)
+    labels[labels == -100] = tokenizer.pad_token_id
+    label_str = tokenizer.batch_decode(labels, skip_special_tokens=True)
 
-    return "".join(preds)
+    scores = {'rouge-1': 0, 'rouge-2': 0, 'rouge-l': 0}
+
+    rouge_output = rouge.get_scores(pred_str, label_str)
+    for row in rouge_output:
+        scores['rouge-1'] += row['rouge-1']['f']
+        scores['rouge-2'] += row['rouge-2']['f']
+        scores['rouge-l'] += row['rouge-l']['f']
+
+    scores = {k: v / len(rouge_output) for k, v in scores.items()}
+
+    return input_str, pred_str, label_str, scores
+    # return {['rouge2'].mid
+    #     "rouge2_precision": round(rouge_output.precision, 4),
+    #     "rouge2_recall": round(rouge_output.recall, 4),
+    #     "rouge2_fmeasure": round(rouge_output.fmeasure, 4),
+    # }
 
 
-with open(args.informal_texts, 'r') as file:
-    sentences = []
-    for line in file:
-        sentences.append(line)
+dic, val_df = read_dataset(args.validation_path, args.dict_path, args.min_count, split=False)
 
+val_dataset = FormalDataset(dic, val_df, args.append_formals)
+val_dataloader = DataLoader(val_dataset, collate_fn=lambda data: collate_fn(data, tokenizer),
+                            batch_size=args.batch_size)
 
-dic = read_dict(args.dict_path, args.min_count)
+d = {'informals': [], 'prediction_formals': [], 'ground_truth_formals': []}
+scores = {'rouge-1': 0, 'rouge-2': 0, 'rouge-l': 0}
+i = 0
+with tqdm(val_dataloader, unit="batch") as tepoch:
+    for batch in tepoch:
+        tepoch.set_description(f"Validation")
 
-if args.append_formals:
-    sentences = find_formal_forms(sentences, dic)
+        batch = {k: v.to(device) for k, v in batch.items()}
+        inputs_str, preds_str, labels_str, rouge_output = convert_compute(**batch)
 
-res = []
-for sentence in sentences:
-    res.append(convert(sentence))
+        scores = {k: scores[k] + v for k, v in rouge_output.items()}
+        i += 1
 
-for r in res:
-    print(r)
+        d['informals'] = d['informals'] + inputs_str
+        d['prediction_formals'] = d['prediction_formals'] + preds_str
+        d['ground_truth_formals'] = d['ground_truth_formals'] + labels_str
+        tepoch.set_postfix(rouge1=rouge_output['rouge-1'], rouge2=rouge_output['rouge-2'],
+                           rougel=rouge_output['rouge-l'])
+
+scores = {k: v / i for k, v in scores.items()}
+print(scores)
+df = pd.DataFrame(d)
+df.to_csv('validation_path.csv')
